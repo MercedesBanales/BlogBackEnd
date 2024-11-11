a340128cd5f1e7f4ed2e1e346144ab4c48e3229c
MercedesBanales <mercibanales@hotmail.com>
2024-11-11 15:17:29 -0300

    fixed bugs

--- src/db.js
+++ src/db.js
@@ -17,7 +17,7 @@ import { MongoClient } from 'mongodb';
 let db;
 
 async function connectToDb(cb) {
-    const client = new MongoClient('mongodb://127.0.0.1:27018');
+    const client = new MongoClient('mongodb://127.0.0.1:27017');
     await client.connect();
 
     db = client.db('react-blog-db');
